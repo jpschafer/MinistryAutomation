44e29cd4750fc9ce56d95ee4175f8c8b57f25e59
Jason Schafer <jason59549@gmail.com>
2024-03-12 11:53:08 -0400

    Update rcl_reg.py

--- rcl/rcl_reg.py
+++ rcl/rcl_reg.py
@@ -4,5 +4,5 @@ import re
 
 with open('rcl_reg.txt', 'r') as rcl_regex_string:
   with open('rcl_reg_test_data.txt', 'r') as rcl_test_string:
-    rcl_regex = re.search( f'({rcl_regex_sring})', rcl_test_string)    
+    result = re.search( f'({rcl_regex_string})', rcl_test_string)    
     print(result.groups())
