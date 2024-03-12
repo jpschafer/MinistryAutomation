704dca6f15c486de2ab2f04e46695e5519348717
Jason Schafer <jason59549@gmail.com>
2024-03-12 12:00:06 -0400

    Update rcl_reg.py

--- rcl/rcl_reg.py
+++ rcl/rcl_reg.py
@@ -3,9 +3,9 @@
 import re
 
 with open('rcl_reg.txt', 'r', encoding="utf-8") as rcl_reg_txt:
-    rcl_reg_string = rcl_reg_txt.read().rstrip();
+    rcl_regex_string = rcl_reg_txt.read().rstrip()
     with open('rcl_reg_test_data.txt', 'r', encoding="utf-8") as rcl_reg_test_data_txt:
-        rcl_reg_test_string =  rcl_reg_test_data_txt.read().rstrip();
-        
+        rcl_test_string = rcl_reg_test_data_txt.read().rstrip()
+
         result = re.search( f'({rcl_regex_string})', rcl_test_string)
         print(result.groups())
