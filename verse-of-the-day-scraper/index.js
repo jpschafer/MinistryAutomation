cefbc9ae08f80952484c7321c776d6ff4b473df2
Jason Schafer <jason59549@gmail.com>
2024-04-06 09:55:05 -0400

    Update index.js
    Added Some Additional Thoughts & Notes

--- verse-of-the-day-scraper/index.js
+++ verse-of-the-day-scraper/index.js
@@ -1,5 +1,7 @@
 // Scrapes a Verse of the Day From YouVersion's Web Page
 // Necessary since there is not an RSS Feed or something cleaner like an API I can hit From 
 // (I'm sure I could reverse-engineer theirs if I 'wanted', but that's a bit too much work, plus I think the web page is generated on the server-side).
-
+// This unfortunately cannot run exclusively on Zapier due to requiring Puppeteer as a 3rd party dependency. Maybe there is a way you could minify and bundle your code
+// to get away with a minified JS to shove in Zapier that has everything together, but I'm not sure how that would work or if a tool like that exists. 
+// Normally things aren't run like that.
 
