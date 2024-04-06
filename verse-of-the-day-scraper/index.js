3b1f498bc55d760263d977ddbd4146089eaa442c
Jason Schafer <jason59549@gmail.com>
2024-04-06 09:52:52 -0400

    Create index.js

---
+++ verse-of-the-day-scraper/index.js
@@ -0,0 +1,5 @@
+// Scrapes a Verse of the Day From YouVersion's Web Page
+// Necessary since there is not an RSS Feed or something cleaner like an API I can hit From 
+// (I'm sure I could reverse-engineer theirs if I 'wanted', but that's a bit too much work, plus I think the web page is generated on the server-side).
+
+
