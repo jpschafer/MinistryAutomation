acdc1e0a94b427527cf545f2786471a7016f5242
Jason Schafer <jason59549@gmail.com>
2024-07-11 10:59:22 -0400

    Formatted & Cleaned Up Some Linting

--- rcl/rcl_reg.py
+++ rcl/rcl_reg.py
@@ -2,10 +2,10 @@
 
 import re
 
-with open('rcl_reg.txt', 'r', encoding="utf-8") as rcl_reg_txt:
+with open("rcl_reg.txt", "r", encoding="utf-8") as rcl_reg_txt:
     rcl_regex_string = rcl_reg_txt.read().rstrip()
-    with open('rcl_reg_test_data.txt', 'r', encoding="utf-8") as rcl_reg_test_data_txt:
+    with open("rcl_reg_test_data.txt", "r", encoding="utf-8") as rcl_reg_test_data_txt:
         rcl_test_string = rcl_reg_test_data_txt.read().rstrip()
 
-        result = re.search( f'({rcl_regex_string})', rcl_test_string)
+        result = re.search(f"({rcl_regex_string})", rcl_test_string)
         print(result.groups())
--- rcl/rcl_zapier.py
+++ rcl/rcl_zapier.py
@@ -1,42 +1,51 @@
-""" Zapier Module that Process RCL Calendar Description to retrieve Non-Psalter Old Testament Reading and Gospel Reading as well as URL of Lectionary Week """
+""" Zapier Module that Process RCL Calendar Description to retrieve 
+Non-Psalter Old Testament Reading and Gospel Reading as well as URL of Lectionary Week """
 
 # Import necessary libraries
 import json
 import re
 import requests
 
+
 def get_pages(user_token):
-    # This code Is "Better" But requires too much time for the Zapier 1 second time limit due to latency. 
-    #url = f'https://graph.facebook.com/v19.0/me/accounts?access_token={user_token}'
-    #print(url)
-    #response = requests.get(url)
-    #return response.json()['data']
+    """Retrieves the Pages and their Access Tokens"""
+    # This code Is "Better" But requires too much time for the Zapier 1 second time limit due to latency.
+    # url = f'https://graph.facebook.com/v19.0/me/accounts?access_token={user_token}'
+    # print(url)
+    # response = requests.get(url)
+    # return response.json()['data']
 
     # Unfortunately hardcode your access_tokens for each page here with their ids so you can loop through them
-    response = [{"id": 123, "access_token": 'abc123'}, {"id": 321, "access_token": "cba321"}] 
-    return response
+    resp = [
+        {"id": 123, "access_token": "abc123"},
+        {"id": 321, "access_token": "cba321"},
+    ]
+    return resp
+
 
 # Defining a function to process the text with a regular expression
 def process_text_with_regex(text):
-    # The regular expression pattern to match the year in the text
-    # It looks for a word boundary, followed by a capital letter 'Y', followed by a colon, 
-    # followed by a space, and then captures one or more word characters as a group
+    """The regular expression pattern to match the year in the text
+    It looks for a word boundary, followed by a capital letter 'Y', followed by a colon,
+    followed by a space, and then captures one or more word characters as a group"""
+
     pattern = r"(?:\** *)((?:[\d ]*[a-zA-Z]+(?: \d*:\d*)?)(?:(?: - )| | (?:-))?(?:(?:(?:\d* )?[a-zA-Z]+ )?\d*(?:[:-]+\d*)?))(?: +)(?:((?:[\d ]*[a-zA-Z]+(?: \d*:\d*)?)(?:(?: - )| |(?:-))?(?:(?:(?:\d* )?[a-zA-Z]+ )?\d*(?:[:-]+\d*)?))(?:(?:(?: +)or(?:[\d ]*[a-zA-Z]+(?: \d*:\d*)?)(?:(?: - )| |(?:-))?(?:(?:(?:\d* )?[a-zA-Z]+ )?\d*(?:[:-]+\d*)?))?))(?: +)((?:[\d ]*[a-zA-Z]+(?: \d*:\d*)?)(?:(?: - )| |(?:-))?(?:(?:(?:\d* )?[a-zA-Z]+ )?\d*(?:[:-]+\d*)?))(?: +)((?:[\d ]*[a-zA-Z]+(?: \d*:\d*)?)(?:(?: - )| |(?:-))?(?:(?:(?:\d* )?[a-zA-Z]+ )?\d*(?:[:-]+\d*)?))(?: +)(https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&//=]*))"
 
     # Using the re.search function to find the first occurrence of the pattern in the text
-    text = text.replace('\n', ' ').replace('\r', '')
-    match = re.search(pattern,  text)
+    text = text.replace("\n", " ").replace("\r", "")
+    match = re.search(pattern, text)
 
     # Checking if a match was found
     if match:
         print(match)
         # If a match was found, returning the matched group (the year)
         return f"{match.group(1)}, {match.group(4)}", match.group(5)
-    else:
-        # If no match was found, returning None
-        return None, None
+    # If no match was found, returning None
+    return None, None
+
 
 def publish_facebook_post(input_data, readings, access_token, url, page_url):
+    """Publishes the Posts to a specified FB page."""
     # Define the headers for the HTTP request
     headers = {
         "Authorization": f"Bearer {access_token}",  # Include the access token in the Authorization header
@@ -51,30 +60,46 @@ def publish_facebook_post(input_data, readings, access_token, url, page_url):
 
     # Send a POST request to the Facebook Graph API endpoint to publish the post
     try:
-        # Add Timeout to beat Zapier 1 second limit since regex takes a little bit. We just need to fire off a submit successfully.
-        response = requests.post(page_url, headers=headers, data=json.dumps(data),timeout=(None, 0.3))
+        # Add Timeout to beat Zapier 1 second limit since regex takes a little bit.
+        # We just need to fire off a submit successfully.
+        resp = requests.post(
+            page_url, headers=headers, data=json.dumps(data), timeout=(None, 0.3)
+        )
     except requests.exceptions.Timeout:
-      print("Timeout occurred")
+        print("Timeout occurred")
 
-    # Define the output as a dictionary with the key "response" and the value as the response from the API
+    # Define the output as a dictionary with the key "response"
+    # and the value as the response from the API
     return {"response": "Some Response"}
 
-# Getting the text from the input data
-text = input_data["text"]
 
-# Calling the function to process the text with the regular expression
-#print(text)
-readings, url = process_text_with_regex(text)
 
-# Printing the processed text
-#print(readings, url)
+def main(data) {
+    # Getting the text from the input data
+    text = input_data["text"]
 
-fb_pages = get_pages(input_data['system_user_token'])
+    # Calling the function to process the text with the regular expression
+    # print(text)
+    readings, url = process_text_with_regex(text)
 
-responses = []
-for page in fb_pages:
-    page_url = f"https://graph.facebook.com/v19.0/{page['id']}/feed"
-    response = publish_facebook_post(input_data, readings, page['access_token'], url, page_url)
-    responses.append(fb_pages)
+    # Printing the processed text
+    # print(readings, url)
 
-output = {"readings": readings, "url": url, "responses": responses}
+    fb_pages = get_pages(input_data["system_user_token"])
+
+    responses = []
+    for page in fb_pages:
+        page_url = f"https://graph.facebook.com/v19.0/{page['id']}/feed"
+        response = publish_facebook_post(
+            input_data, readings, page["access_token"], url, page_url
+        )
+        responses.append(fb_pages)
+
+    output = {"readings": readings, "url": url, "responses": responses}
+}
+
+
+if (input_data) {
+    
+    } # type: ignore
+ 
